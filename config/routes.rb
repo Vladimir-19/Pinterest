20e95e31454f78fe204223a4062479b914bed755
Vladimir-19 <v.s.solovey@gmail.com>
2020-12-01 12:59:52 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :boards, only: [:create, :index, :show, :update, :destroy]
+    resources :pins, only: [:create, :index, :show, :update, :destroy]
+    resources :joinsboardspins, only: [:create, :destroy]
+  end
+
+  root to: "static_pages#root"
 end
