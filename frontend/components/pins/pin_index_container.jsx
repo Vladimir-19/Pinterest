1c162688fadbc93bb377aa51f81c18dadb277313
Vladimir-19 <v.s.solovey@gmail.com>
2020-12-29 23:39:42 -0500

    pin

--- frontend/components/pins/pin_index_container.jsx
+++ frontend/components/pins/pin_index_container.jsx
@@ -1,54 +1,50 @@
-import { connect } from "react-redux";
-import PinIndex from "./pin_index";
+// import { connect } from "react-redux";
+// import PinIndex from "./pin_index";
 
-import { fetchPins } from "../../actions/pin_actions";
-import { openModal } from "../../actions/modal_actions";
-
-const mapStateToProps = (state, ownProps) => {
-    return {
-        page: ownProps.page,
-        pins: ownProps.pins,
-        currentUserId: state.session.id,
-        user: state.entities.users[state.session.id]
-    }
-};
-
-const mapDispatchToProps = dispatch => ({
-    fetchPins: () => dispatch(fetchPins()),
-    openEditPin: pinId => dispatch(openModal("edit-pin", pinId)),
-    openNewBoardPin: pinId => dispatch(openModal("new-board-pin", pinId))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(PinIndex);
-
-// import { connect } from 'react-redux';
-
-// import { openModal } from '../../actions/modal_actions';
-
-// import { fetchPins } from '../../actions/pin_actions';
-
-// import PinIndex from './pin_index';
-
+// import { fetchPins } from "../../actions/pin_actions";
+// import { openModal } from "../../actions/modal_actions";
 
 // const mapStateToProps = (state, ownProps) => {
-//     const currentUser = state.entities.users[state.session.id];
-//     const modal = state.ui.modal;
-//     // const searchPins = Object.values(ownProps);
-//     const pins = Object.values(state.entities.pins)
 //     return {
-//         // searchPins: searchPins,
-//         pins: pins,
-//         modal: modal,
-//         currentUser: currentUser
+//         page: ownProps.page,
+//         pins: ownProps.pins,
+//         currentUserId: state.session.id,
+//         user: state.entities.users[state.session.id]
 //     }
 // };
 
 // const mapDispatchToProps = dispatch => ({
-//     openModal: modal => dispatch(openModal(modal)),
-//     fetchPins: () => dispatch(fetchPins())
+//     fetchPins: () => dispatch(fetchPins()),
+//     openEditPin: pinId => dispatch(openModal("edit-pin", pinId)),
+//     openNewBoardPin: pinId => dispatch(openModal("new-board-pin", pinId))
 // });
 
-// export default connect(
-//     mapStateToProps,
-//     mapDispatchToProps
-// )(PinIndex);
+// export default connect(mapStateToProps, mapDispatchToProps)(PinIndex);
+
+import { connect } from 'react-redux';
+import { openModal } from '../../actions/modal_actions';
+import { fetchPins } from '../../actions/pin_actions';
+import PinIndex from './pin_index';
+
+const mapStateToProps = (state, ownProps) => {
+    const currentUser = state.entities.users[state.session.id];
+    const modal = state.ui.modal;
+    // const searchPins = Object.values(ownProps);
+    const pins = Object.values(state.entities.pins)
+    return {
+        // searchPins: searchPins,
+        pins: pins,
+        modal: modal,
+        currentUser: currentUser
+    }
+};
+
+const mapDispatchToProps = dispatch => ({
+    openModal: modal => dispatch(openModal(modal)),
+    fetchPins: () => dispatch(fetchPins())
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(PinIndex);
