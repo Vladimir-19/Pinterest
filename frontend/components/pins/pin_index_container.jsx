862e60298a613a384477236d53ac356bbc7eab1b
Vladimir-19 <v.s.solovey@gmail.com>
2021-01-14 23:47:18 -0500

    pin

--- frontend/components/pins/pin_index_container.jsx
+++ frontend/components/pins/pin_index_container.jsx
@@ -1,11 +1,28 @@
-// import { connect } from "react-redux";
-// import PinIndex from "./pin_index";
+// import {
+//     connect
+// } from 'react-redux';
 
-// import { fetchPins } from "../../actions/pin_actions";
-// import { openModal } from "../../actions/modal_actions";
+// import {
+//     openModal
+// } from '../../actions/modal_actions';
+
+// import { fetchPins } from '../../actions/pin_actions';
+
+// import PinIndex from './pin_index';
+
 
 // const mapStateToProps = (state, ownProps) => {
+//     const currentUser = state.entities.users[state.session.id];
+//     const modal = state.ui.modal;
+//     // const searchPins = Object.values(ownProps);
+//     const pins = Object.values(state.entities.pins)
 //     return {
+//         // // searchPins: searchPins,
+//         pins: pins,
+//         modal: modal,
+//         currentUser: currentUser,
+//         // currentUser: state.session.id,
+//         // user: state.entities.users[state.session.id]
 //         page: ownProps.page,
 //         pins: ownProps.pins,
 //         currentUserId: state.session.id,
@@ -14,40 +31,15 @@
 // };
 
 // const mapDispatchToProps = dispatch => ({
+//     openModal: modal => dispatch(openModal(modal)),
 //     fetchPins: () => dispatch(fetchPins()),
-//     openEditPin: pinId => dispatch(openModal("edit-pin", pinId)),
-//     openNewBoardPin: pinId => dispatch(openModal("new-board-pin", pinId))
+//     openNewBoardPin:  pinId => dispatch(openModal("new-board-pin", pinId))
 // });
 
-// export default connect(mapStateToProps, mapDispatchToProps)(PinIndex);
-
-                        // import { connect } from 'react-redux';
-                        // import { openModal } from '../../actions/modal_actions';
-                        // import { fetchPins } from '../../actions/pin_actions';
-                        // import PinIndex from './pin_index';
-
-                        // const mapStateToProps = (state, ownProps) => {
-                        //     const currentUser = state.entities.users[state.session.id];
-                        //     const modal = state.ui.modal;
-                        //     // const searchPins = Object.values(ownProps);
-                        //     const pins = Object.values(state.entities.pins)
-                        //     return {
-                        //         // searchPins: searchPins,
-                        //         pins: pins,
-                        //         modal: modal,
-                        //         currentUser: currentUser
-                        //     }
-                        // };
-
-                        // const mapDispatchToProps = dispatch => ({
-                        //     openModal: modal => dispatch(openModal(modal)),
-                        //     fetchPins: () => dispatch(fetchPins())
-                        // });
-
-                        // export default connect(
-                        //     mapStateToProps,
-                        //     mapDispatchToProps
-                        // )(PinIndex);
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+// )(PinIndex);
 
 import {
     connect
@@ -65,26 +57,19 @@ import PinIndex from './pin_index';
 const mapStateToProps = (state, ownProps) => {
     const currentUser = state.entities.users[state.session.id];
     const modal = state.ui.modal;
-    // const searchPins = Object.values(ownProps);
+    const searchPins = Object.values(ownProps);
     const pins = Object.values(state.entities.pins)
     return {
-        // // searchPins: searchPins,
+        searchPins: searchPins,
         pins: pins,
         modal: modal,
-        currentUser: currentUser,
-        // currentUser: state.session.id,
-        // user: state.entities.users[state.session.id]
-        page: ownProps.page,
-        pins: ownProps.pins,
-        currentUserId: state.session.id,
-        user: state.entities.users[state.session.id]
+        currentUser: currentUser
     }
 };
 
 const mapDispatchToProps = dispatch => ({
     openModal: modal => dispatch(openModal(modal)),
-    fetchPins: () => dispatch(fetchPins()),
-    openNewBoardPin:  pinId => dispatch(openModal("new-board-pin", pinId))
+    fetchPins: () => dispatch(fetchPins())
 });
 
 export default connect(
--- frontend/components/pins/pin_index_item.jsx
+++ frontend/components/pins/pin_index_item.jsx
@@ -1,131 +1,131 @@
-// import React from 'react';
-// import { withRouter } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
 
+// import React from "react";
+// import { Link, withRouter } from "react-router-dom";
+
+
+// const masonryEvents = ["load", "resize"];
 
 // class PinIndexItem extends React.Component {
 //     constructor(props) {
 //         super(props);
-//         this.handleClick = this.handleClick.bind(this);
+//         this.state = this.props.pin; // was c
+
+//         this.resizeGridItem = this.resizeGridItem.bind(this); //was c
+//         // this.handleClick = this.handleClick.bind(this);
 //     }
 
-//     handleClick(e) {
-//         e.preventDefault();
-//         this.props.openModal({ modal: 'pinboard', pinId: this.props.pin.id })
+//     resizeGridItem() { //was c
+//         let item = document.getElementById(this.state.id);
+//         let grid = document.getElementById('grid');
+//         let rowHeight = parseInt(window.getComputedStyle(document.getElementById('grid')).getPropertyValue('grid-auto-rows'));
+//         let rowGap = parseInt(window.getComputedStyle('grid').getPropertyValue('grid-row-gap'));
+//         let itemImg = item.querySelector(".masonry-image");
+//         let rowSpan = Math.ceil((itemImg.getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+//         // if (this.state.title !== '') rowSpan += 2;
+//         item.style.gridRowEnd = "span " + rowSpan;
+//     }
+
+//     componentDidMount(e) { //was c
+//         setTimeout(() => this.resizeGridItem(), 2000);
+//         masonryEvents.forEach((e) => window.addEventListener(e, this.resizeGridItem));
+//         // e.preventDefault();
+//         // this.props.openModal({ modal: 'pinboard', pinId: this.props.pin.id })
 //     }
 
 //     render() {
-//         const { pin, openModal } = this.props;
+//         const { userId, user, page, pin, openEditPin, openNewBoardPin } = this.props;
+
+//         const pinTitle = (userId !== null && page === "home" && pin.title !== "") ? (
+//             <div className="pin-index-item title-container">
+//                 <div className="pin-index-item title">{pin.title}</div>
+//             </div>
+//         ) : null;
+
+//         const editPinLink = (page === 'profile' && location.hash.includes(user.username)) ? ( //location.hash.includes(user.username)
+//             <a
+//                 className="pin-index-item edit-pin-link"
+//                 onClick={() => openEditPin(pin.id)}
+//             >
+//                 <i className="fas fa-pencil-alt edit-pin-icon"></i>
+//             </a>
+//         ) : null;
+
+//         const openBoardPinLink = (
+//             <a
+//                 className="pin-index-item save-board-pin-link"
+//                 onClick={() => openNewBoardPin(pin.id)}
+//             >
+//                 <div className="pin-index-item save-board-pin-text">Save</div>
+//             </a>
+//         );
+
+//         const pinUrl = pin.url;
+//         const shortPinUrl = pinUrl //.slice(12, 22) + "...";
+//         const pinLink = (pinUrl !== '') ? (
+//             <a href={`${pinUrl}`} target="_blank" className="pin-index-item pin-link">
+//                 <i className="fas fa-external-link-alt pin-link-icon"></i>
+//                 <div className="pin-index-item pin-link-text">
+//                     {shortPinUrl}
+//                 </div>
+//             </a>
+//         ) : null;
 
 //         return (
-//             <Link className='pin-links' to={`pins/${pin.id}`}>
-//                 <img className="masonry-pin-item" src={pin.photoUrl} />
-//                 <p className='pin-index-title'>{pin.title}</p>
-//             </Link>
+//             // <div id={`${this.state.id}`} className="pin-index-item container">
+//             <div className="pin-index-item container">
+//                 <div className="pin-index-item masonry-item">
+//                     <Link
+//                     key={pin.id}
+//                         // to={`/pin/${pin.id}`}
+//                         to={`/pins/${pin.id}`}
+//                         className="pin-index-item pin-show-link"
+//                     >
+//                         <div className="pin-index-item overlay"></div>
+//                         <img
+//                             src={pin.photo} 
+//                             // src={pin.photoUrl}
+//                             className="pin-index-item masonry-image" />
+//                     </Link>
+//                     <div className="pin-index-item links">
+//                         <div className="pin-index-item edit-pin-link-container">{editPinLink}</div>
+//                         <div className="save-board-pin-link-container">{openBoardPinLink}</div>
+//                         <div className="pin-index-item pin-link-container">{pinLink}</div>
+//                     </div>
+//                 </div>
+//                 {pinTitle}
+//             </div>
 //         );
 //     }
-// }
+// };
 
-// export default withRouter(PinIndexItem);
+// export default PinIndexItem;
 
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import React from "react";
-import { Link, withRouter } from "react-router-dom";
-
-
-const masonryEvents = ["load", "resize"];
 
 class PinIndexItem extends React.Component {
     constructor(props) {
         super(props);
-        this.state = this.props.pin; // was c
-
-        this.resizeGridItem = this.resizeGridItem.bind(this); //was c
-        // this.handleClick = this.handleClick.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    resizeGridItem() { //was c
-        let item = document.getElementById(this.state.id);
-        let grid = document.getElementById('grid');
-        let rowHeight = parseInt(window.getComputedStyle(document.getElementById('grid')).getPropertyValue('grid-auto-rows'));
-        let rowGap = parseInt(window.getComputedStyle('grid').getPropertyValue('grid-row-gap'));
-        let itemImg = item.querySelector(".masonry-image");
-        let rowSpan = Math.ceil((itemImg.getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
-        // if (this.state.title !== '') rowSpan += 2;
-        item.style.gridRowEnd = "span " + rowSpan;
-    }
-
-    componentDidMount(e) { //was c
-        setTimeout(() => this.resizeGridItem(), 2000);
-        masonryEvents.forEach((e) => window.addEventListener(e, this.resizeGridItem));
-        // e.preventDefault();
-        // this.props.openModal({ modal: 'pinboard', pinId: this.props.pin.id })
+    handleClick(e) {
+        e.preventDefault();
+        this.props.openModal({ modal: 'pinboard', pinId: this.props.pin.id })
     }
 
     render() {
-        const { userId, user, page, pin, openEditPin, openNewBoardPin } = this.props;
-
-        const pinTitle = (userId !== null && page === "home" && pin.title !== "") ? (
-            <div className="pin-index-item title-container">
-                <div className="pin-index-item title">{pin.title}</div>
-            </div>
-        ) : null;
-
-        const editPinLink = (page === 'profile' && location.hash.includes(user.username)) ? ( //location.hash.includes(user.username)
-            <a
-                className="pin-index-item edit-pin-link"
-                onClick={() => openEditPin(pin.id)}
-            >
-                <i className="fas fa-pencil-alt edit-pin-icon"></i>
-            </a>
-        ) : null;
-
-        const openBoardPinLink = (
-            <a
-                className="pin-index-item save-board-pin-link"
-                onClick={() => openNewBoardPin(pin.id)}
-            >
-                <div className="pin-index-item save-board-pin-text">Save</div>
-            </a>
-        );
-
-        const pinUrl = pin.url;
-        const shortPinUrl = pinUrl //.slice(12, 22) + "...";
-        const pinLink = (pinUrl !== '') ? (
-            <a href={`${pinUrl}`} target="_blank" className="pin-index-item pin-link">
-                <i className="fas fa-external-link-alt pin-link-icon"></i>
-                <div className="pin-index-item pin-link-text">
-                    {shortPinUrl}
-                </div>
-            </a>
-        ) : null;
+        const { pin, openModal } = this.props;
 
         return (
-            // <div id={`${this.state.id}`} className="pin-index-item container">
-            <div className="pin-index-item container">
-                <div className="pin-index-item masonry-item">
-                    <Link
-                    key={pin.id}
-                        // to={`/pin/${pin.id}`}
-                        to={`/pins/${pin.id}`}
-                        className="pin-index-item pin-show-link"
-                    >
-                        <div className="pin-index-item overlay"></div>
-                        <img
-                            src={pin.photo} 
-                            // src={pin.photoUrl}
-                            className="pin-index-item masonry-image" />
-                    </Link>
-                    <div className="pin-index-item links">
-                        <div className="pin-index-item edit-pin-link-container">{editPinLink}</div>
-                        <div className="save-board-pin-link-container">{openBoardPinLink}</div>
-                        <div className="pin-index-item pin-link-container">{pinLink}</div>
-                    </div>
-                </div>
-                {pinTitle}
-            </div>
+            <Link className='pin-links' to={`pins/${pin.id}`}>
+                <img className="masonry-pin-item" src={pin.photoUrl} />
+                <p className='pin-index-title'>{pin.title}</p>
+            </Link>
         );
     }
-};
+}
 
-export default PinIndexItem;
+export default withRouter(PinIndexItem);
