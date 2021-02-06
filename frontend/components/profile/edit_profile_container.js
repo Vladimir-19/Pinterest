11ccb981c75312a3f0d7c08f6c84880d7449e31f
Vladimir-19 <v.s.solovey@gmail.com>
2021-02-06 10:05:38 -0700

    edit_profile_container

---
+++ frontend/components/profile/edit_profile_container.js
@@ -0,0 +1,31 @@
+import {
+    connect
+} from 'react-redux';
+
+import {
+    openModal, closeModal
+} from '../../actions/modal_actions';
+
+
+import EditProfile from './edit_profile';
+import { updateUser, fetchUser } from '../../actions/user_actions';
+
+
+const mapStateToProps = (state) => ({
+    currentUser: state.entities.users[state.session.id],
+    pins: Object.values(state.entities.pins),
+    boards: Object.values(state.entities.boards),
+    errors: state.errors.session,
+});
+
+const mapDispatchToProps = dispatch => ({
+    openModal: modal => dispatch(openModal(modal)),
+    closeModal: () => dispatch(closeModal()),
+    updateUser: (user, id) => dispatch(updateUser(user, id)),
+    fetchUser: userId => dispatch(fetchUser(userId))
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(EditProfile);
