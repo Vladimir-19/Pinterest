6042940808d7ba7a916e26079f3dcc6f841c5be3
Vladimir-19 <v.s.solovey@gmail.com>
2021-04-30 16:38:54 -0400

    application rb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module NewApp
     # config.middleware.use ActionDispatch::Cookies
     # config.middleware.use ActionDispatch::Session::CookieStore
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
